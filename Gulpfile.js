333e24b54aab1addd3571127a7235fb6ea184aa7
balogunofafrica <balogunakanbi.k@gmail.com>
2023-11-02 08:10:59 +0100

    refactor: remove jekyll

--- Gulpfile.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-const shell = require('gulp-shell');
-
-gulp.task('clear-cache', shell.task('rm -rf _site'));
-
-gulp.task('build', gulp.series('clear-cache', shell.task('jekyll build')));
