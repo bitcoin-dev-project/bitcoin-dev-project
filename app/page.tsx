03935f261db99486516ead35a804d8f088f9abf3
balogunofafrica <balogunakanbi.k@gmail.com>
2023-11-04 10:05:29 +0100

    feat(wip): landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,196 @@
-import Image from 'next/image'
+import Accordion from "@/components/accordion";
+import Button from "@/components/button";
+import SectionCard from "@/components/section-card";
+import ToolsCard from "@/components/tools-card";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="max-w-[100%]">
+      <div className="flex justify-between md:flex-col">
+        <div className="flex flex-col justify-end">
+          <div className="flex flex-col gap-y-[56px] pl-14 lg:pl-0">
+            <h1 className="text-[62px] lg:text-[36px] font-bold leading-normal">
+              Learn, contribute <br></br> and build on{" "}
+              <span className="text-orange">bitcoin</span> <br></br>
+              and <span className="text-green">lightning</span>
+            </h1>
+            <div>
+              <ul className="list-disc text-2xl lg:text-lg leading-[150%]">
+                <li>Help build the future of money </li>
+                <li>Challenge yourself & go down various rabbit holes</li>
+                <li>
+                  Build applications and infrastructure that have meaning and
+                  impact
+                </li>
+              </ul>
+            </div>
+            <div className="flex justify-between gap-x-3 lg:flex-col lg:gap-y-4">
+              <div className="flex flex-col w-[186px] gap-y-2 text-center ">
+                <Button href="#learn" className="w-full">
+                  Learn
+                </Button>
+                <p>Guided journey for development on bitcoin and lightning</p>
+              </div>
+
+              <div className="flex flex-col w-[186px] gap-y-2 text-center">
+                <Button href="#contribute" className="w-full">
+                  Contribute
+                </Button>
+                <p>
+                  Resources to teach you to contribute to bitcoin & lightning
+                </p>
+              </div>
+
+              <div className="flex flex-col w-[186px] gap-y-2 text-center">
+                <Button href="#build" className="w-full">
+                  Build
+                </Button>
+                <p>Hands-on and theoretical supports to build applications</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative h-[664px] w-[690px] md:w-full bg-green">
+          <Image src="/hero.jpg" alt="Hero" fill priority />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex flex-col px-14">
+        <h2
+          id="learn"
+          className="mt-[100px] text-5xl font-semibold text-center self-center mb-6"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Learn
+        </h2>
+        <div className="flex justify-between md:flex-col md:gap-y-4">
+          <SectionCard
+            alt="bitcoin-infra-development"
+            body="Learning resources for your journey to become a Bitcoin Core developer."
+            btnText="Learn"
+            src="/bitcoin-infra-development.jpg"
+            title="Bitcoin Infrastructure Development"
+          />
+          <SectionCard
+            alt="lighting-infra-development"
+            body="Learning resources for your journey to becoming a Infrastructure developer."
+            btnText="Learn"
+            src="/lighting-infra-development.jpg"
+            title="Lightning Infrastructure Development"
+          />
+          <SectionCard
+            alt="lighting-app-development"
+            body="Discover the wide range of applications you can build with lightning."
+            btnText="Learn"
+            src="/lighting-app-development.jpg"
+            title="Lightning Infrastructure Development"
+          />
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="flex mt-32 md:flex-col md:gap-y-4">
+          <div className="flex flex-col gap-y-6">
+            <h2 id="build" className="text-5xl font-semibold text-center">
+              Build
+            </h2>
+            <SectionCard
+              alt="lighting-app-development"
+              body="Discover the wide range of applications you can build with lightning."
+              btnText="Learn"
+              src="/build.jpg"
+              title="Lightning Infrastructure Development"
+              className="w-[95%]"
+            />
+          </div>
+          <div className="flex flex-col gap-y-6">
+            <h2 id="contribute" className="text-5xl font-semibold text-center">
+              Contribute
+            </h2>
+            <SectionCard
+              alt="lighting-app-development"
+              body="Discover the wide range of applications you can build with lightning."
+              btnText="Learn"
+              src="/contribute.jpg"
+              title="Lightning Infrastructure Development"
+              className="w-[95%]"
+            />
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
+        <div className="mt-32 flex flex-col self-center text-center gap-y-2">
+          <h2 className="text-5xl font-semibold">Tools to Explore</h2>
+          <p className="text-xl leading-[140%]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            accumsan tempus nunc, nec posuere magna sagittis euismod. Vestibulum
+            sed justo ullamcorper, pharetra leo eget, accumsan quam. Fusce
+            efficitur leo nisi, sed elementum arcu molestie
           </p>
-        </a>
+          <div className="relative flex flex-col w-full min-h-[420px] xl:h-[600px] md:h-[1000px]">
+            <Image alt="explore-map" src="/explore-map.jpg" fill />
+            <div className="px-[80px] pb-10 absolute bottom-0 left-0 right-0 flex flex-col">
+              <div className="flex justify-between xl:flex-col xl:items-center xl:gap-y-6">
+                <div className="flex gap-x-10 md:flex-col md:gap-y-4">
+                  <ToolsCard
+                    alt=""
+                    name="Bitcoin Transcripts"
+                    src="/chat-btc.jpg"
+                  />
+                  <ToolsCard
+                    alt=""
+                    name="Bitcoin Transcripts"
+                    src="/saving-satoshi.jpg"
+                  />
+                </div>
+                <div className="flex gap-x-10 md:flex-col md:gap-y-4">
+                  <ToolsCard
+                    alt=""
+                    name="Bitcoin Transcripts"
+                    src="/bitcoin-transcript.jpg"
+                  />
+                  <ToolsCard
+                    alt=""
+                    name="Bitcoin Search"
+                    src="/bitcoin-search.jpg"
+                  />
+                </div>
+              </div>
+              <Button
+                href="#"
+                className="mt-[12px] w-[212px] md:w-[100px] self-center"
+              >
+                Explore
+              </Button>
+              {/* <Link
+                href="#"
+                className=" flex items-center justify-center py-4 text-[20px] bg-gradient-btn rounded-md text-white self-center"
+              >
+                Explore
+              </Link> */}
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="mt-32 flex flex-col self-center text-center mb-8 gap-y-5">
+          <h2 className="text-5xl font-semibold md:text-3xl">FAQs</h2>
+          <div className="flex flex-col gap-y-8">
+            <Accordion
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum nemo necessitatibus quaerat rerum officiis nesciunt praesentium, id, itaque voluptatibus a tempora fuga laboriosam modi blanditiis incidunt earum. Tempore, deserunt?"
+              title="I'm not a dev but I want to learn more about bitcoin"
+            />
+            <Accordion
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum nemo necessitatibus quaerat rerum officiis nesciunt praesentium, id, itaque voluptatibus a tempora fuga laboriosam modi blanditiis incidunt earum. Tempore, deserunt?"
+              title="I'd like to learn how to become a dev"
+            />
+            <Accordion
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum nemo necessitatibus quaerat rerum officiis nesciunt praesentium, id, itaque voluptatibus a tempora fuga laboriosam modi blanditiis incidunt earum. Tempore, deserunt?"
+              title="Where do I learn about funding in bitcoin and FOSS?"
+            />
+            <Accordion
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum nemo necessitatibus quaerat rerum officiis nesciunt praesentium, id, itaque voluptatibus a tempora fuga laboriosam modi blanditiis incidunt earum. Tempore, deserunt?"
+              title="What is the meaning of life?"
+            />
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
