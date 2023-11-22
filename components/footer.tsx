b02e1d2c3db4ef2e0868a2f0a3689757aa68be12
balogunofafrica <balogunakanbi.k@gmail.com>
2023-11-22 13:01:13 +0100

    fix: footer copy

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-import React from "react";
-
 export default function Footer() {
   return (
     <footer className="mt-10 relative bottom-0 bg-pale-orange flex items-center justify-center p-[24px] text-black">
-      <p>Built with by ❤️ bitcoiners</p>
+      <p>Built with ❤️ by bitcoiners</p>
     </footer>
   );
 }
