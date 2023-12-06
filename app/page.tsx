1a28320070fafe931d6416376883047e11449e06
balogunofafrica <balogunakanbi.k@gmail.com>
2023-12-06 02:43:29 +0000

    increase quality of hero image

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,7 @@ export default function Home() {
             fill
             priority
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            quality={100}
           />
         </div>
       </div>
