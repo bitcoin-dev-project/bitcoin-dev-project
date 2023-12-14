18a47ed2ba4ef1562e9af542cbaf41e048a0bbf9
Jonas <jonas@chaincode.com>
2023-12-14 18:25:49 -0500

    update footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <footer className="mt-10 relative bottom-0 bg-pale-orange flex items-center justify-center p-[24px] text-black">
-      <p>Built with ❤️ by bitcoiners</p>
+      <p>Built with ❤️ by{" "}
+      <a className="text-orange" href="https://chaincode.com" target="_blank" rel="noopener noreferrer">Chaincode Labs</a>
+      {" "}x{" "}
+      <a className="text-orange" href="https://bitcoin.design/" target="_blank" rel="noopener noreferrer">Bitcoin Design Community</a>
+      </p>
     </footer>
   );
 }
