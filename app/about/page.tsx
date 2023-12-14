8ae2094eb6ba8709c8465736e239dc2cd116c5ba
Jonas <jonas@chaincode.com>
2023-12-14 18:12:13 -0500

    update copy on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,12 @@
-import Accordion from "@/components/accordion";
-import { faqs } from "@/content/data";
-
 type Props = {
-  content: string;
+  content: string | React.ReactNode;
   title: string;
 };
 
 const Item = ({ content, title }: Props) => (
   <div className="gap-2">
-    <li className="list-disc font-medium text-xl md:text-base">{title}</li>
-    <p className="text-lg md:text-sm">{content}</p>
+    <h2 className="text-2xl md:text-lg font-medium leading-normal">{title}</h2>
+    <p className="text-lg md:text-md">{content}</p>
   </div>
 );
 
@@ -20,54 +17,56 @@ export default function About() {
         <h1 className="text-5xl md:text-lg md:text-center font-bold leading-normal">
           About The Bitcoin Dev Project
         </h1>
-        <ul className="flex flex-col gap-5">
-          <Item
-            content="The Bitcoin Dev Project is a collaborative and
-          open-source initiative that aims to demystify and advance Bitcoin and
-          Lightning Network technologies. By focusing on Free and Open Source
-          Software (FOSS), we are building a community where developers and
-          enthusiasts can learn, contribute, and create a solid foundation for
-          the future of money."
-            title="Empowering Innovation"
-          />
-          <Item
-            content="We believe in a future where financial systems are open,
-            transparent, and accessible to everyone. The Bitcoin Dev Project
-            serves as a hub for education and development, fostering a space where
-            individuals can dive into the complexities of the bitcoin technology
-            and emerge as innovators in the field."
-            title="Our Vision"
-          />
-          <Item
-            content="Our journey is
-            about continuous learning, building, and sharing knowledge with a
-            growing global community."
-            title="Our Journey"
-          />
-          <Item
-            content="Whether you're a seasoned
-            developer or just starting, there's a place for you in The Bitcoin Dev
-            Project. Explore our projects, interact with educational content, and
-            join the journey toward a decentralized financial future."
-            title="Get Involved"
-          />
-          <Item
-            content="Join our efforts to shape the financial technology landscape. Dive
-            into our resources, contribute to our projects, and take part in
-            discussions that push the boundaries of what's possible with Bitcoin
-            and the Lightning Network."
-            title="Connect with Us"
-          />
-        </ul>
-      </div>
-
-      <div className="my-32 flex flex-col self-center text-center mb-8 gap-y-5">
-        <h2 className="text-5xl font-semibold md:text-3xl">FAQs</h2>
-        <div className="flex flex-col gap-y-8">
-          {faqs.map((faq) => (
-            <Accordion key={faq.title} {...faq} />
-          ))}
-        </div>
+        <Item
+          content={
+            <>
+              The Bitcoin Dev Project is a{" "} 
+              <a className="text-orange" href="https://chaincode.com" target="_blank" rel="noopener noreferrer">Chaincode Labs</a>{" "}
+              sponsored initiative geared towards developers at the beginning of their bitcoin journey. Chaincode has a{" "}
+              <a className="text-orange" href="https://bluematt.bitcoin.ninja/2016/08/08/chaincode/" target="_blank" rel="noopener noreferrer"> long history</a>{" "}of providing technical education through its{" "}
+              <a className="text-orange" href="https://residency.chaincode.com" target="_blank" rel="noopener noreferrer">in-person residencies</a>
+              {" "}and started offering online programs via the{" "}
+              <a className="text-orange" href="https://chaincode.com" target="_blank" rel="noopener noreferrer">seminars</a>{" "}in 2020.
+              <br />
+              <br />
+              There is an oft-repeated sentiment in the community that bitcoin does not need you. While bitcoin is designed to be resilient, we do need you. Bitcoin needs all the talent and energy it can gather to solve some of the most difficult technical problems of our time. Bitcoin in your hands changes everything.
+              <br />
+              <br />
+              Our goal is to provide newcomers with the resources and support your bitcoin free open source software (FOSS) development journey. Make no mistake, the goal of this project is to convince you to contribute to{" "}
+              <a className="text-orange" href="/projects">bitcoin FOSS projects</a>
+              . We measure our success by action, not passive consumption of educational materials.
+            </>
+          }
+          title="What"
+        />
+        <Item
+          content={
+            <>
+              <></>
+              <ul>
+                <li>
+                  Design by{" "}
+                  <a className="text-orange" href="https://twitter.com/MogashniNaidoo" target="_blank" rel="noopener">Mogashni Naidoo</a>
+                  {" "}and{" "}
+                  <a className="text-orange" href="https://twitter.com/StephenDeLorme" target="_blank" rel="noopener">Stephen DeLorme</a>
+                </li>
+                <li>
+                  Product by{" "}
+                  <a className="text-orange" href="https://twitter.com/aassoiants" target="_blank" rel="noopener">Art Assoiants</a>
+                </li>
+                <li>
+                  Dev by{" "}
+                  <a className="text-orange" href="https://twitter.com/balogunofafrica" target="_blank" rel="noopener">Balogun</a>
+                </li>
+                <li>
+                  Ideas by{" "}
+                  <a className="text-orange" href="https://twitter.com/adamcjonas" target="_blank" rel="noopener">Adam Jonas</a>
+                </li>
+              </ul>
+            </>
+          }
+          title="Who"
+        />
       </div>
     </main>
   );
