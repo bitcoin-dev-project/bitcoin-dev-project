cc19fc896d4b8e159c6c651c374581b61e6a2424
balogunofafrica <balogunakanbi.k@gmail.com>
2023-12-16 07:22:43 +0100

    fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,25 @@
 export default function Footer() {
   return (
-    <footer className="mt-10 relative bottom-0 bg-pale-orange flex items-center justify-center p-[24px] text-black">
-      <p>Built with ❤️ by{" "}
-      <a className="text-orange" href="https://chaincode.com" target="_blank" rel="noopener noreferrer">Chaincode Labs</a>
-      {" "}x{" "}
-      <a className="text-orange" href="https://bitcoin.design/" target="_blank" rel="noopener noreferrer">Bitcoin Design Community</a>
+    <footer className="mt-10 relative bottom-0 text-center bg-pale-orange flex items-center justify-center p-[24px] text-black">
+      <p>
+        Built with ❤️ by{" "}
+        <a
+          className="text-orange"
+          href="https://chaincode.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Chaincode Labs
+        </a>{" "}
+        x{" "}
+        <a
+          className="text-orange"
+          href="https://bitcoin.design/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bitcoin Design Community
+        </a>
       </p>
     </footer>
   );
