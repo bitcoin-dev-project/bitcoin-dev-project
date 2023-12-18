86bdcba3245f603d25a53f1caf03db2db0a3aa85
balogunofafrica <balogunakanbi.k@gmail.com>
2023-12-18 09:01:11 +0100

    shrink about and career page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,29 +12,71 @@ const Item = ({ content, title }: Props) => (
 
 export default function About() {
   return (
-    <main className="p-14 sm:p-7 mx-auto my-0">
-      <div className="flex flex-col md:items-center mb-24 gap-y-10">
+    <main className="flex flex-col p-14 sm:p-7 mx-auto my-0">
+      <div className="flex flex-col mb-24 gap-y-10 w-3/5 md:w-full self-center">
         <h1 className="text-5xl md:text-lg md:text-center font-bold leading-normal">
           About The Bitcoin Dev Project
         </h1>
         <Item
           content={
             <>
-              The Bitcoin Dev Project is a{" "} 
-              <a className="text-orange" href="https://chaincode.com" target="_blank" rel="noopener noreferrer">Chaincode Labs</a>{" "}
-              sponsored initiative geared towards developers at the beginning of their bitcoin journey. Chaincode has a{" "}
-              <a className="text-orange" href="https://bluematt.bitcoin.ninja/2016/08/08/chaincode/" target="_blank" rel="noopener noreferrer"> long history</a>{" "}of providing technical education through its{" "}
-              <a className="text-orange" href="https://residency.chaincode.com" target="_blank" rel="noopener noreferrer">in-person residencies</a>
-              {" "}and started offering online programs via the{" "}
-              <a className="text-orange" href="https://chaincode.com" target="_blank" rel="noopener noreferrer">seminars</a>{" "}in 2020.
+              The Bitcoin Dev Project is a{" "}
+              <a
+                className="text-orange"
+                href="https://chaincode.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Chaincode Labs
+              </a>{" "}
+              sponsored initiative geared towards developers at the beginning of
+              their bitcoin journey. Chaincode has a{" "}
+              <a
+                className="text-orange"
+                href="https://bluematt.bitcoin.ninja/2016/08/08/chaincode/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                long history
+              </a>{" "}
+              of providing technical education through its{" "}
+              <a
+                className="text-orange"
+                href="https://residency.chaincode.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                in-person residencies
+              </a>{" "}
+              and started offering online programs via the{" "}
+              <a
+                className="text-orange"
+                href="https://chaincode.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                seminars
+              </a>{" "}
+              in 2020.
               <br />
               <br />
-              There is an oft-repeated sentiment in the community that bitcoin does not need you. While bitcoin is designed to be resilient, we do need you. Bitcoin needs all the talent and energy it can gather to solve some of the most difficult technical problems of our time. Bitcoin in your hands changes everything.
+              There is an oft-repeated sentiment in the community that bitcoin
+              does not need you. While bitcoin is designed to be resilient, we
+              do need you. Bitcoin needs all the talent and energy it can gather
+              to solve some of the most difficult technical problems of our
+              time. Bitcoin in your hands changes everything.
               <br />
               <br />
-              Our goal is to provide newcomers with the resources and support your bitcoin free open source software (FOSS) development journey. Make no mistake, the goal of this project is to convince you to contribute to{" "}
-              <a className="text-orange" href="/projects">bitcoin FOSS projects</a>
-              . We measure our success by action, not passive consumption of educational materials.
+              Our goal is to provide newcomers with the resources and support
+              your bitcoin free open source software (FOSS) development journey.
+              Make no mistake, the goal of this project is to convince you to
+              contribute to{" "}
+              <a className="text-orange" href="/projects">
+                bitcoin FOSS projects
+              </a>
+              . We measure our success by action, not passive consumption of
+              educational materials.
             </>
           }
           title="What"
@@ -46,21 +88,56 @@ export default function About() {
               <ul>
                 <li>
                   Design by{" "}
-                  <a className="text-orange" href="https://twitter.com/MogashniNaidoo" target="_blank" rel="noopener">Mogashni Naidoo</a>
-                  {" "}and{" "}
-                  <a className="text-orange" href="https://twitter.com/StephenDeLorme" target="_blank" rel="noopener">Stephen DeLorme</a>
+                  <a
+                    className="text-orange"
+                    href="https://twitter.com/MogashniNaidoo"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    Mogashni Naidoo
+                  </a>{" "}
+                  and{" "}
+                  <a
+                    className="text-orange"
+                    href="https://twitter.com/StephenDeLorme"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    Stephen DeLorme
+                  </a>
                 </li>
                 <li>
                   Product by{" "}
-                  <a className="text-orange" href="https://twitter.com/aassoiants" target="_blank" rel="noopener">Art Assoiants</a>
+                  <a
+                    className="text-orange"
+                    href="https://twitter.com/aassoiants"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    Art Assoiants
+                  </a>
                 </li>
                 <li>
                   Dev by{" "}
-                  <a className="text-orange" href="https://twitter.com/balogunofafrica" target="_blank" rel="noopener">Balogun</a>
+                  <a
+                    className="text-orange"
+                    href="https://twitter.com/balogunofafrica"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    Balogun
+                  </a>
                 </li>
                 <li>
                   Ideas by{" "}
-                  <a className="text-orange" href="https://twitter.com/adamcjonas" target="_blank" rel="noopener">Adam Jonas</a>
+                  <a
+                    className="text-orange"
+                    href="https://twitter.com/adamcjonas"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    Adam Jonas
+                  </a>
                 </li>
               </ul>
             </>
--- app/career/page.tsx
+++ app/career/page.tsx
@@ -14,8 +14,8 @@ const Item = ({ content, title }: Props) => (
 
 export default function Career() {
   return (
-    <main className="p-14 sm:p-7 mx-auto my-0">
-      <div className="flex flex-col md:items-center mb-24 gap-y-10">
+    <main className="flex flex-col p-14 sm:p-7 mx-auto my-0">
+      <div className="flex flex-col mb-24 gap-y-10 w-3/5 md:w-full self-center">
         <h1 className="text-5xl md:text-lg md:text-center font-bold leading-normal">
           Start Your Career in Bitcoin Open Source Development
         </h1>
@@ -23,14 +23,48 @@ export default function Career() {
           content={
             <>
               <p className="my-4">
-                Funding for Free Open Source Software (FOSS) projects is a historical problem that goes back to the beginning of FOSS. Bitcoin funding is different from other open-source and other cryptocurrency projects. Getting a grant in bitcoin is pretty straightforward. You either need someone to vouch for you or you need to do work - ideally both. While grant programs often have open applications, the secret to getting funding is not much of a secret. Start doing the job for free. It establishes you as a contributor and proves your motivation to do the work. It shows that you are a good investment. Applicants will have much more success if they do the work and then apply. For most jobs, the applicant is trying to convince the employer that they are capable of doing the job. But the job-seeker has no idea what the work or environment is actually like. In open-source, one does not have to guess. Do the work. Demonstrate capability. Then ask for support.
+                Funding for Free Open Source Software (FOSS) projects is a
+                historical problem that goes back to the beginning of FOSS.
+                Bitcoin funding is different from other open-source and other
+                cryptocurrency projects. Getting a grant in bitcoin is pretty
+                straightforward. You either need someone to vouch for you or you
+                need to do work - ideally both. While grant programs often have
+                open applications, the secret to getting funding is not much of
+                a secret. Start doing the job for free. It establishes you as a
+                contributor and proves your motivation to do the work. It shows
+                that you are a good investment. Applicants will have much more
+                success if they do the work and then apply. For most jobs, the
+                applicant is trying to convince the employer that they are
+                capable of doing the job. But the job-seeker has no idea what
+                the work or environment is actually like. In open-source, one
+                does not have to guess. Do the work. Demonstrate capability.
+                Then ask for support.
               </p>
               <p className="my-4">
-                Doing the work also means demonstrating your work. Not all work in open-source work is as visible as writing code. The less visible work is not less valuable, but it is in your best interest to create artifacts of your effort. Transparency is your friend. If you learn something, it is helpful to codify it by writing a blog post or keeping a running log. I have seen some write a bi-weekly summary email tracking their progress. Code reviews used to be hard to track, but now GitHub does a better job crediting a green square. Taking long walks to reflect on how to approach a problem is necessary, but writing up your conclusions in a public place is valuable collateral that can forever serve as evidence of your progress.
+                Doing the work also means demonstrating your work. Not all work
+                in open-source work is as visible as writing code. The less
+                visible work is not less valuable, but it is in your best
+                interest to create artifacts of your effort. Transparency is
+                your friend. If you learn something, it is helpful to codify it
+                by writing a blog post or keeping a running log. I have seen
+                some write a bi-weekly summary email tracking their progress.
+                Code reviews used to be hard to track, but now GitHub does a
+                better job crediting a green square. Taking long walks to
+                reflect on how to approach a problem is necessary, but writing
+                up your conclusions in a public place is valuable collateral
+                that can forever serve as evidence of your progress.
               </p>
               <p>
-                (Excerpt from {" "}
-                <a className="text-orange" href="https://chaincode.com" target="_blank" rel="noopener noreferrer">A guide for bitcoin open-source grant seekers</a>)
+                (Excerpt from{" "}
+                <a
+                  className="text-orange"
+                  href="https://chaincode.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  A guide for bitcoin open-source grant seekers
+                </a>
+                )
               </p>
             </>
           }
@@ -42,20 +76,56 @@ export default function Career() {
               <></>
               <ul className="my-4">
                 <li>
-                  <a className="text-orange" href="https://spiral.xyz/#grants" target="_blank" rel="noopener noreferrer">Spiral</a>{" "}is the Bitcoin/LN R&D arm of Block who have been distributing grants since 2019.
+                  <a
+                    className="text-orange"
+                    href="https://spiral.xyz/#grants"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Spiral
+                  </a>{" "}
+                  is the Bitcoin/LN R&D arm of Block who have been distributing
+                  grants since 2019.
                 </li>
                 <li>
-                  <a className="text-orange" href="https://brink.homerun.co/grants" target="_blank" rel="noopener noreferrer">Brink</a>{" "}is a 501c3, mostly focused on funding Bitcoin Core developers, established in 2020.
+                  <a
+                    className="text-orange"
+                    href="https://brink.homerun.co/grants"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Brink
+                  </a>{" "}
+                  is a 501c3, mostly focused on funding Bitcoin Core developers,
+                  established in 2020.
                 </li>
                 <li>
-                  <a className="text-orange" href="https://opensats.org/apply" target="_blank" rel="noopener noreferrer">OpenSats</a>{" "}is a 501c3, established in 2021.
+                  <a
+                    className="text-orange"
+                    href="https://opensats.org/apply"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    OpenSats
+                  </a>{" "}
+                  is a 501c3, established in 2021.
                 </li>
                 <li>
-                  <a className="text-orange" href="https://forms.monday.com/forms/57019f8829449d9e729d9e3545a237ea" target="_blank" rel="noopener noreferrer">Human Rights Foundation</a>{" "}is a 501c3 that has been distributing grants since 2020.
-                </li>                
+                  <a
+                    className="text-orange"
+                    href="https://forms.monday.com/forms/57019f8829449d9e729d9e3545a237ea"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Human Rights Foundation
+                  </a>{" "}
+                  is a 501c3 that has been distributing grants since 2020.
+                </li>
               </ul>
               <p className="my-4">
-                Various exchanges and individuals have sponsored devs in the past, but the above orgs have become the main distributors of grants over the last couple of years.
+                Various exchanges and individuals have sponsored devs in the
+                past, but the above orgs have become the main distributors of
+                grants over the last couple of years.
               </p>
             </>
           }
@@ -66,10 +136,17 @@ export default function Career() {
             <>
               <></>
               <p className="my-4">
-                If you are ready to begin a career in Bitcoin FOSS, Chaincode Labs is hosting a three-month program to put you in the best position to acheive your goals. {" "}
-                <span className="font-bold">DEADLINE TO APPLY IS DECEMBER 31st, 2023.</span>
+                If you are ready to begin a career in Bitcoin FOSS, Chaincode
+                Labs is hosting a three-month program to put you in the best
+                position to acheive your goals.{" "}
+                <span className="font-bold">
+                  DEADLINE TO APPLY IS DECEMBER 31st, 2023.
+                </span>
               </p>
-              <Button href="https://learning.chaincode.com/#FOSS" className="w-full">
+              <Button
+                href="https://learning.chaincode.com/#FOSS"
+                className="w-full"
+              >
                 APPLY
               </Button>
             </>
