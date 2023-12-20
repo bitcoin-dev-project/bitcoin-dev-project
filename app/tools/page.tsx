550ecb51e6d32494a8967bb9ce85cd0ded0bbe8d
Jonas <jonas@chaincode.com>
2023-12-19 21:46:51 -0500

    spacing and title sizes

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -6,7 +6,7 @@ export default function Explore() {
   return (
     <main className="p-14 sm:p-7 mx-auto my-0">
       <div className="flex flex-col md:items-center mb-24 md:mb-12 gap-y-10">
-        <h1 className="text-[62px] lg:text-[36px] md:text-center font-bold leading-tight">
+        <h1 className="text-[58px] lg:text-[36px] md:text-center font-bold leading-tight">
           {tools.title}
         </h1>
         <p className="text-2xl md:text-xl md:text-center max-w-[60vw] md:max-w-full">
