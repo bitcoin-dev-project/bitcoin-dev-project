a2ccf21ce28bd615703720b66101ea56fdf8b7eb
Jonas <jonas@chaincode.com>
2023-12-21 13:14:24 -0500

    attempt landscape file change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,16 @@ import { Banner } from "@/components/banner";
 
 export const metadata: Metadata = {
   title: "The Bitcoin Dev Project",
-  description: "Find your way in bitcoin open source development",
+  description: "Find your way in bitcoin open source",
   openGraph: {
     images: ["https://bitcoindevs.xyz/hero.jpg"],
     title: "The Bitcoin Dev Project",
     url: "https://bitcoindevs.xyz",
-    description: "Find your way in bitcoin open source development",
+    description: "Find your way in bitcoin open source",
   },
   twitter: {
-    images: ["https://bitcoindevs.xyz/square-hero.jpg"],
-    card: "summary",
+    images: ["https://bitcoindevs.xyz/landscape_2_1.jpg?v1"],
+    card: "summary_large_image",
   },
   keywords:
     "bitcoin, lightning, bitcoin development, bitcoin open source, bitcoin FOSS, bitcoin career, free open source software, open source, bitcoin development, bitcoin development community, bitcoin development resources, bitcoin development tools, bitcoin development guides, bitcoin development courses",
