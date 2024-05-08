459ab35564516364bbff37d2534186890cc5069f
Theophilus <isahtheophilus@gmail.com>
2024-05-08 12:56:34 +0100

    chore: remove unused comments and files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -43,19 +43,14 @@ const config: Config = {
     },
     screens: {
       xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
 
       lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
 
       md: { max: "767px" },
-      // => @media (max-width: 767px) { ... }
 
       sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
 
       xs: { max: "500px" },
-      // => @media (max-width: 500px) { ... }
     },
   },
   plugins: [],
