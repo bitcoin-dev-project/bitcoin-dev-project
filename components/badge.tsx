9fecbfc8ad9225dfd91ddcf13c4e08265283e5a2
IgboPharaoh <solomoneze62@gmail.com>
2024-06-03 19:25:03 +0100

    feat: add filter by language to badge

--- components/badge.tsx
+++ components/badge.tsx
@@ -4,11 +4,13 @@ import clsx from "clsx"
 const Badge = ({
     name,
     className,
-    keys
+    keys,
+    addFilterParam
 }: {
     name: string
     className?: string
     keys: string[]
+    addFilterParam: (key: string, value: string) => void
 }) => {
     const isSelected = keys
         ?.map((value) => value.toLowerCase())
@@ -17,9 +19,10 @@ const Badge = ({
     return (
         <div
             className={clsx(
-                `inline-flex w-fit items-center whitespace-nowrap rounded-full font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-gray-100 text-gray-600 hover:bg-gray-100/80 px-2 py-1 text-xs ${isSelected ? "border-gray-600 text-black bg-gray-200 border-[1.5px]" : ""}`,
+                `inline-flex w-fit items-center whitespace-nowrap rounded-full font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-gray-100 text-gray-600 hover:bg-gray-100/80 px-2 py-1 text-xs cursor-pointer ${isSelected ? "border-gray-600 text-black bg-gray-200 border-[1.5px]" : ""}`,
                 className
             )}
+            onClick={() => addFilterParam("languages", name.toLowerCase())}
         >
             {name}
         </div>
