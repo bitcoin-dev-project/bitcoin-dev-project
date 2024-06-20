59bb759a6d9ba5516da190acdb57fe6bc5f05eea
Theophilus <60826700+Extheoisah@users.noreply.github.com>
2024-06-20 19:22:06 +0100

    chore: update page metadata for Good First Issues page (#72)

--- app/good-first-issues/page.tsx
+++ app/good-first-issues/page.tsx
@@ -1,7 +1,29 @@
+import { Metadata } from "next"
 import React from "react"
 
 import RepositoryIssues from "@/components/repository-issues"
 import { getRepoIssues } from "@/lib/get-repo-issues"
+
+export const metadata: Metadata = {
+    title: "Good First Issues | Bitcoin Dev Project",
+    keywords: "bitcoin, open source, good first issues, bitcoin development",
+    description:
+        "Explore Good First Issues from vetted Bitcoin open-source projects and start making impactful contributions today!",
+    openGraph: {
+        images: ["https://bitcoindevs.xyz/good-first-issues.png"],
+        title: "Good First Issues",
+        url: "https://bitcoindevs.xyz/good-first-issues",
+        description:
+            "Explore Good First Issues from vetted Bitcoin open-source projects and start making impactful contributions today!"
+    },
+    twitter: {
+        images: ["https://bitcoindevs.xyz/good-first-issues.png?v1"],
+        card: "summary_large_image",
+        title: "Good First Issues",
+        description:
+            "Explore Good First Issues from vetted Bitcoin open-source projects and start making impactful contributions today!"
+    }
+}
 
 const Contribute = async () => {
     const issues = await getRepoIssues()
