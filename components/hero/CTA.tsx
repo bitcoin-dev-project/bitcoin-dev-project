8dea8e0bed4ba3389c25180fb027cf4d8de60610
jrakibi <j.errakibi@gmail.com>
2024-07-16 13:33:04 +0100

    Landing copy updates (#87)
    * update landing page copy
    
    * remove React fragment

--- components/hero/CTA.tsx
+++ components/hero/CTA.tsx
@@ -16,7 +16,7 @@ export default function CTA() {
                     </p>
                     <div className="mt-10 flex items-center justify-center gap-x-6 ">
                         <a
-                            href="/bitcoin-core"
+                            href="https://discord.com/invite/EAy9XMufbY"
                             className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-800 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                         >
                             Get started
--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image"
-
-import { hero } from "@/content/landing"
-
 import Link from "next/link"
+import { hero } from "@/content/landing"
 import { Container } from "./Container"
 
 export function Hero() {
@@ -10,26 +8,23 @@ export function Hero() {
         <Container className="pb-16 pt-20 max-sm:pb-0 text-center lg:pt-32 bg-white dark:bg-black">
             <h1 className="mx-auto max-w-4xl font-display text-6xl font-semibold tracking-tight text-black-800 dark:text-black-100 sm:text-7xl lg:text-8xl">
                 <span className="relative text-orange-500">
-                    <span className="relative"> BUILD </span>
+                    <span className="relative"> Build </span>
                 </span>
                 the future
                 <span className="block whitespace-nowrap">of money</span>
             </h1>
-
             <p className="mx-auto mt-6 max-w-2xl text-2xl max-sm:text-base tracking-tight text-black-700 dark:text-black-300">
-                Study and contribute to Bitcoin open-source development
+                Study and contribute to bitcoin open-source development
             </p>
             <div className="mt-10 flex justify-center gap-x-6">
                 <Link href="/bitcoin-core">
                     <button
                         type="button"
-                        className="py-1 pl-4 duration-200 pr-2 border-orange-500 bg-orange-500 hover:text-black h-12 hover:bg-white text-white justify-between uppercase border-2 hover:border-white rounded-full inline-flex items-center gap-12"
+                        className="py-1 pl-4 pr-2 duration-200 border-orange-500 bg-orange-500 hover:text-black h-12 hover:bg-white text-white justify-between uppercase border-2 hover:border-white rounded-full inline-flex items-center gap-12"
                     >
-                        Start Learning
+                        Learn Bitcoin
                         <div className="bg-black h-8 w-8 text-white inline-flex items-center justify-center rounded-full">
-                            {" "}
                             <span>
-                                {" "}
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="w-5 h-5"
@@ -40,16 +35,24 @@ export function Hero() {
                                         d="m18 6l-1.43 1.393L24.15 15H4v2h20.15l-7.58 7.573L18 26l10-10L18 6z"
                                     ></path>
                                 </svg>
-                            </span>{" "}
+                            </span>
                         </div>
+                    </button>
+                </Link>
+                <Link href="/good-first-issues">
+                    <button
+                        type="button"
+                        className="py-1 pl-4 pr-4 duration-200 border-orange-500 bg-transparent text-orange-500 hover:bg-orange-500 hover:text-white h-12 uppercase border rounded-full inline-flex items-center justify-center transition-colors duration-200"
+                    >
+                        Contribute to Open Source
                     </button>
                 </Link>
             </div>
             <div className="max-sm:mt-0 mt-36 lg:mt-44">
                 <p className="font-display text-base text-black-800 dark:text-black-100">
-                    We're building products for Bitcoiners
+                    Explore products we’ve built to make bitcoin tech more
+                    accessible
                 </p>
-
                 {hero.map((group, groupIndex) => (
                     <section key={groupIndex}>
                         <ul
