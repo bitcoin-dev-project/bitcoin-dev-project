b7b94a98a97a5422285e16416f63f08d86686052
jrakibi <j.errakibi@gmail.com>
2024-08-08 14:19:21 +0800

    Remove suppressHydrationWarning property

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang={siteMetadata.language} suppressHydrationWarning>
+        <html lang={siteMetadata.language}>
             <head>
                 <script
                     async
