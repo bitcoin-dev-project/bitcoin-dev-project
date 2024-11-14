34f13c7122fce1bb7d66f5c04e6fdcad06c4d7cd
jrakibi <j.errakibi@gmail.com>
2024-11-14 19:30:05 +0800

    feat: integrate BDP UI library footer into BDP website

---
+++ components/footer-component.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import React from "react"
+import { Footer } from "@bitcoin-dev-project/bdp-ui"
+import Link from "next/link"
+// import { menuApps } from "@/utils/data";
+import Image from "next/image"
+
+function Wrapper({
+    className,
+    ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+    return (
+        <div
+            className={`w-full max-w-[1920px] px-4 lg:px-10 2xl:px-[60px] mx-auto ${className}`}
+            {...props}
+        />
+    )
+}
+
+const FooterComponent = () => {
+    return (
+        <div className="flex flex-col items-center justify-center w-full">
+            <div className="border-b-[0.5px] border-b-gray-custom-400 max-md:mx-6 w-full"></div>
+            <Wrapper className="py-10 max-md:pt-8 max-md:pb-[67px] max-lg:px-2 w-full">
+                <Footer>
+                    <Footer.Socials
+                        platforms={[
+                            {
+                                entity: "github",
+                                entityLink:
+                                    "https://github.com/bitcoin-dev-project/bitcoin-dev-project",
+                                iconProps: {
+                                    className: "hover:text-orange-400"
+                                }
+                            },
+                            {
+                                entity: "discord",
+                                entityLink: "https://discord.gg/EAy9XMufbY",
+                                iconProps: {
+                                    className: "hover:text-orange-400"
+                                }
+                            },
+                            {
+                                entity: "twitter",
+                                entityLink: "https://twitter.com/Bitcoin_Devs",
+                                iconProps: {
+                                    className: "hover:text-orange-400"
+                                }
+                            },
+                            {
+                                entity: "nostr",
+                                entityLink:
+                                    "https://njump.me/npub10p33xu03t8q7d9nxtks63dq4gmt4v4d3ppd5lcdp4rg9hxsd0f8q7mn2l2",
+                                iconProps: {
+                                    className: "hover:text-orange-400"
+                                }
+                            }
+                        ]}
+                    />
+                    <Footer.Public dashboardLink="https://visits.bitcoindevs.xyz/share/El4tCqIKLIhJIq9y/bitcoin-dev-project" />
+                    <Footer.About
+                        entityLink="https://bitcoindevs.xyz"
+                        entityName="Bitcoin Dev Project"
+                    />
+                    <Footer.Feedback feedbackLink="https://cryptpad.fr/form/#/2/form/view/3P2CsohsHOkcH7C+WdtX0-tvqjBHqXnAmz5D9yx0e04" />
+                </Footer>
+            </Wrapper>
+        </div>
+    )
+}
+
+export default FooterComponent
