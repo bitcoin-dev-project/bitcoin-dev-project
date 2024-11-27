9344a8441a2dfe26a763aac099b68e080aeddf16
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-11-27 17:14:38 +0100

    fix(version): bdp-ui version update

--- components/banner/BossBanner.tsx
+++ components/banner/BossBanner.tsx
@@ -1,17 +1,24 @@
 "use client"
 
 import { Banner } from "@bitcoin-dev-project/bdp-ui"
+import { usePathname } from "next/navigation"
 
 const BossBanner = () => {
+    const pathname = usePathname()
+    const isHomePage = pathname === "/"
+
     return (
-        <div className={`w-full dark:bg-black bg-white sticky top-[66px] z-20`}>
+        <div
+            className={`w-full dark:bg-black bg-white sticky top-[60px] lg:top-[64px] z-20 ${isHomePage ? "block" : "hidden"}`}
+        >
             <Banner
                 headingText="Start your career in bitcoin open source —"
                 linkText="APPLY TODAY"
                 linkTo="https://learning.chaincode.com/#BOSS"
                 hasBoss
                 styles={{
-                    link: "dark:text-orange-400 text-orange-500"
+                    link: "dark:text-orange-400 text-orange-500",
+                    boss: "dark:text-orange-400 text-orange-500"
                 }}
             />
         </div>
