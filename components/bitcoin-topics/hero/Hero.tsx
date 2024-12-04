477b44503134d246d4571aa7bc74504a1b12d59f
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-12-04 15:48:37 +0100

    Add Boss banner (#130)
    * chore(banner): add Boss banner
    
    * fix(version): bdp-ui version update
    
    * chore(banner): added banner to good first issues
    
    * fix(design): overflow in decoding and z-index issue with menu
    
    * refactor(BaseLayout): remove hardcoded top styling on mobile navigation
    
    ---------
    
    Co-authored-by: Emmanuel-Develops <itakpeemma@gmail.com>

--- components/bitcoin-topics/hero/Hero.tsx
+++ components/bitcoin-topics/hero/Hero.tsx
@@ -98,7 +98,7 @@ const ContinueReadingComp: React.FC<{
     }, [])
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-vscode-background-light to-vscode-background-light dark:from-vscode-background-dark dark:to-vscode-background-dark">
+        <div className="min-h-screen overflow-hidden flex items-center justify-center bg-gradient-to-br from-vscode-background-light to-vscode-background-light dark:from-vscode-background-dark dark:to-vscode-background-dark">
             <div className="w-full max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-24 items-center relative">
                     {/* Background elements */}
