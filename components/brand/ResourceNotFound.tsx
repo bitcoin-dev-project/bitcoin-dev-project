b3d4a471d840e5a6e71a164eb2007984cd041dad
sahanaxyz <kbsahana2007@gmail.com>
2026-02-11 02:33:38 +0530

    fix: not found card size with resource cards
    Signed-off-by: sahanaxyz <kbsahana2007@gmail.com>

--- components/brand/ResourceNotFound.tsx
+++ components/brand/ResourceNotFound.tsx
@@ -3,16 +3,15 @@ import Image from "next/image"
 
 const ResourceNotFound: React.FC = () => {
     return (
-        <div className="flex flex-col items-center justify-center w-full py-10">
-            <div className="flex flex-col items-center">
-                <div className="w-[28rem] h-[28rem] relative">
-                    <Image
-                        src="/images/hedgehog-not-found.png"
-                        alt="Hedgehog mascot - no resources found"
-                        fill
-                        className="object-contain"
-                    />
-                </div>
+        <div className="learn-cards-container grid md:grid-cols-2 justify-between w-full gap-5">
+            <div className="border border-brand-stroke-on-base p-4 pb-[22px] rounded-2xl bg-brand-card-bg w-full md:max-w-[375px] flex items-center overflow-hidden">
+                <Image
+                    src="/images/hedgehog-not-found.png"
+                    alt="Hedgehog mascot - no resources found"
+                    width={600}
+                    height={600}
+                    className="w-full h-auto scale-125"
+                />
             </div>
         </div>
     )
