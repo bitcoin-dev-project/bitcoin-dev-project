ad7e6720bbde9526df249589f8562acb88ba07c0
priyashuu <priyaaashu4@gmail.com>
2026-02-11 15:38:36 +0530

    Refactor code structure for improved readability and maintainability

--- components/good-first-issues-footer.tsx
+++ components/good-first-issues-footer.tsx
@@ -8,32 +8,9 @@ const GoodFirstIssuesFooter = () => {
                min-h-[944px] md:min-h-[1000px] lg:min-h-[766px] 
                pt-20 lg:pt-[129px] 
                bg-black bg-cover bg-bottom bg-no-repeat
-               bg-[url('/images/mobiledarkfooter.png')] md:bg-[url('/images/darkfooter.png')]"
+               bg-[url('/images/mobilebgdarkfooter.webp')] md:bg-[url('/images/bgdarkfooter.webp')]"
         >
-            <div
-                className="pointer-events-none absolute inset-x-0
-             bottom-0 md:top-0 md:bottom-auto
-             md:-translate-y-24 lg:-translate-y-40
-             z-0 flex justify-center"
-            >
-                <Image
-                    src="/images/mobiledarkpango.png"
-                    alt=""
-                    width={1440}
-                    height={900}
-                    sizes="100vw"
-                    className="w-full h-full object-contain lg:hidden"
-                />
-                <Image
-                    src="/images/darkpango.png"
-                    alt=""
-                    width={1440}
-                    height={900}
-                    sizes="100vw"
-                    className="hidden w-full h-full object-contain md:block"
-                />
-            </div>
-
+               
             {/* CONTENT */}
             <div className="relative z-10 flex flex-col p-5 lg:flex-row w-full gap-10 max-w-[583px] lg:mx-auto justify-between font-quicksand font-bold text-white">
                 {FOOTERLINKS.map((link) => (
