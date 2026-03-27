1ddf1842f0fa99b12b8ded5b33fff56c9586d289
DARSHAN-R-DARSHAN <darshanramanji07@gmail.com>
2026-03-27 23:43:08 +0530

    Added drop-down for the navbar

--- components/good-first-issue.tsx
+++ components/good-first-issue.tsx
@@ -5,6 +5,7 @@ import BDPLogoDark from "./assets/BDPLogoDark"
 import BDPMobileIcon from "./assets/BDPMobileIcon"
 import BDPMenu from "./assets/BDPMenu"
 import React, { Dispatch, SetStateAction } from "react"
+import { ChevronDown } from "lucide-react"
 
 const GoodFirstIssuesNavbar = ({
     setIsOpen
@@ -27,12 +28,42 @@ const GoodFirstIssuesNavbar = ({
                 >
                     About
                 </Link>
-                <Link
-                    href="/explore"
-                    className="px-3 capitalize py-3 bg-brand-orange-100 text-white font-bold rounded-[10px] hover:opacity-90 transition-all duration-200 text-base leading-none"
-                >
-                    Start exploring
-                </Link>
+                <div className="relative group">
+                    {/* Button */}
+                    <button className="px-3 py-3 bg-brand-orange-100 text-white font-bold rounded-[10px] transition-all duration-200 text-base leading-none flex items-center gap-1 font-quicksand hover:opacity-90">
+                        Start Exploring
+                        <ChevronDown
+                            className="w-4 h-4 mt-1 ml-0.5 transition-transform duration-200 group-hover:rotate-180"
+                            strokeWidth={3.5}
+                        />
+                    </button>
+
+                    {/* Dropdown */}
+                    <div className="absolute right-0 mt-2 w-44 rounded-xl bg-black opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200">
+                        <div className="flex flex-col py-2 text-base font-quicksand">
+                            <Link
+                                href="/learn"
+                                className="px-4 py-2 hover:text-brand-orange-100 transition-colors"
+                            >
+                                Learn
+                            </Link>
+
+                            <Link
+                                href="/contribute"
+                                className="px-4 py-2 hover:text-brand-orange-100 transition-colors"
+                            >
+                                Contribute
+                            </Link>
+
+                            <Link
+                                href="/get-funded"
+                                className="px-4 py-2 hover:text-brand-orange-100 transition-colors"
+                            >
+                                Get Funded
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
             <BDPMenu onClick={onOpen} className="text-white lg:hidden" />
         </nav>
